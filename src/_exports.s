06ac7e7ef871df93da6e4beacb036b353bf5554e
Ray Bellis <ray@isc.org>
2018-10-03 09:55:48 +0100

    updated symbols

--- src/_exports.s
+++ src/_exports.s
@@ -2,17 +2,20 @@
 ; MOS Entry points
 ;
 
-.export FRED	= $fc00
-.export JIM	= $fd00
-.export SHEILA	= $fe00
+.export NMI	   = $0d00
 
-.export OSFIND	= $ffce
-.export OSBGET	= $ffd7
-.export OSFILE	= $ffdd
-.export OSRDCH	= $ffe0
-.export OSASCI	= $ffe3
-.export OSNEWL	= $ffe7
-.export OSWRCH	= $ffee
-.export OSWORD	= $fff1
-.export OSBYTE	= $fff4
-.export OSCLI	= $fff7
+.export OSCLI_VEC  = $0208
+.export OSBYTE_VEC = $020a
+.export OSWORD_VEC = $020c
+.export OSWRCH_VEC = $020e
+.export OSRDCH_VEC = $0210
+.export OSFILE_VEC = $0212
+.export OSARGS_VEC = $0214
+.export OSBGET_VEC = $0216
+.export OSBPUT_VEC = $0218
+.export OSGBPB_VEC = $021a
+.export OSFIND_VEC = $021c
+
+.export FRED	   = $fc00
+.export JIM	   = $fd00
+.export SHEILA	   = $fe00
