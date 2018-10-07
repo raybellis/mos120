8f807b907c93eab5b8d40a0ef207807f1aa60185
Ray Bellis <ray@isc.org>
2018-10-07 14:28:57 +0100

    update scripts

--- bin/compile.sh
+++ bin/compile.sh
@@ -4,5 +4,5 @@ file=mos120.s
 name=`basename $file .s`
 
 cat src/*.s > ${file}
-ca65 -U $file -o ${name}.o -l ${name}.lst
+ca65 -U -o ${name}.o -l ${name}.lst ${file}
 ld65 -t bbc -o ${name}.rom ${name}.o
